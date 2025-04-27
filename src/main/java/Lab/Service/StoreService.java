bab59d5488ddbe3ca64c63114d4247b93d81390b
RazaDon Willrich <razradical93@gmail.com>
2025-04-27 04:51:12 +0000

    Completed Spring JPA CRUD.

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -2,11 +2,21 @@ package Lab.Service;
 
 import Lab.Model.Store;
 import Lab.Repository.StoreRepository;
+
+import org.apache.catalina.StoreManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 public class StoreService {
     StoreRepository storeRepository;
@@ -19,15 +29,18 @@ public class StoreService {
      * @param store a transient store
      * @return the persisted store
      */
+
+     @Transactional
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
+
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -36,13 +49,13 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
-        return null;
+        return storeRepository.findById(id).orElse(null);
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        storeRepository.deleteById(id);
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -50,7 +63,13 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
-        return null;
-    }
+        Optional<Store> existingStoreOptional = storeRepository.findById(id);
 
+        if (existingStoreOptional.isPresent()) {
+            Store existingStore = existingStoreOptional.get();
+            return storeRepository.save(existingStore);
+        } else {
+            return null;
+        }
+    }
 }
